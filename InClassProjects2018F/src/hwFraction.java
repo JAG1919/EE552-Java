39f1f48322a29dbdbd452c443af3a1fc87083293
Justin <justin@Justins-MacBook-Pro.local>
2019-03-10 20:52:51 -0400

    local save

--- InClassProjects2018F/src/hwFraction.java
+++ InClassProjects2018F/src/hwFraction.java
@@ -1,20 +1,47 @@
 public class hwFraction {
+    private int num, den;
+    
+    hwFraction(int a, int b){
+        num = a;
+        den = b;
+    }
+    
+    @Override
+    public String toString() {
+        return num + "/" + den;
+    }
 
-
-	public static void main(String[] args) {
-		hwFraction f1(1,2);
-		// Euclid's algorithm: not required
-		hwFraction f2(2,4); // 1/2   call gcd(2,4), find out = 2 divide num, den by 2
-		hwFraction f3 = f1.add(f2);
-		System.out.println(f1 + " + " + f2 + " = " + f3);
-
-		hwFraction f4 = f1.sub(f2);
-		System.out.println(f1 + " - " + f2 + " = " + f4);
-		
-		hwFraction f5 = f1.mult(f2);
-		System.out.println(f1 + " * " + f2 + " = " + f5);
-
-		hwFraction f6 = f1.neg(); // return -1/2 f1 is unchanged
+    public hwFraction add(hwFraction x) {
+            return new hwFraction(num*x.den + x.num*den, x.den*den);
 	}
+	
+	public hwFraction sub(hwFraction x) {
+		return new hwFraction(num*x.den - x.num*den, x.den*den);
+	}
+
+	public hwFraction mult(hwFraction x) {
+		return new hwFraction(num*x.num, den*x.den);
+	}
+
+	public hwFraction neg() {
+		return new hwFraction(-num,den);
+	}
+
+    public static void main(String[] args) {
+			hwFraction f1 = new hwFraction(1,2);
+
+			hwFraction f2 = new hwFraction(2, 4);
+            hwFraction f3 = f1.add(f2);
+            System.out.println(f1 + " + " + f2 + " = " + f3);
+
+           hwFraction f4 = f1.sub(f2);
+           System.out.println(f1 + " - " + f2 + " = " + f4);
+
+           hwFraction f5 = f1.mult(f2);
+           System.out.println(f1 + " * " + f2 + " = " + f5);
+
+		   hwFraction f6 = f1.neg(); // return -1/2 f1 is unchanged
+		   System.out.println(f6);
+    }
 
 }
